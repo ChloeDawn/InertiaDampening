7250e3e6ae12c8efa485c59a254e871dd4abd29d
InsomniaKitten <admin@designdevils.co.uk>
2019-04-14 22:59:30 +0100

    Update to 1.14 Pre-Release 2

--- src/main/java/io/github/insomniakitten/inertiadampening/mixin/LivingEntityMixin.java
+++ src/main/java/io/github/insomniakitten/inertiadampening/mixin/LivingEntityMixin.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2018 InsomniaKitten
+ * Copyright (C) 2019 InsomniaKitten
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -16,40 +16,30 @@
 
 package io.github.insomniakitten.inertiadampening.mixin;
 
-import net.minecraft.client.input.Input;
-import net.minecraft.client.network.ClientPlayerEntity;
-import net.minecraft.entity.Entity;
+import io.github.insomniakitten.inertiadampening.InertiaDampening;
 import net.minecraft.entity.LivingEntity;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.At.Shift;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(LivingEntity.class)
-@SuppressWarnings("ConstantConditions")
-abstract class LivingEntityMixin extends Entity {
+abstract class LivingEntityMixin {
   private LivingEntityMixin() {
-    super(null, null);
+    throw new UnsupportedOperationException();
   }
 
   @Inject(
+    id = InertiaDampening.INJECTION_ID,
     method = "updateMovement",
     at = @At(
-      value = "INVOKE_STRING",
-      target = "Lnet/minecraft/util/profiler/Profiler;push(Ljava/lang/String;)V",
-      args = "ldc=ai"
+      value = "INVOKE",
+      target = "Lnet/minecraft/entity/LivingEntity;setVelocity(DDD)V",
+      shift = Shift.AFTER
     )
   )
-  private void resetVelocity(final CallbackInfo ci) {
-    if ((Object) this instanceof ClientPlayerEntity) {
-      final ClientPlayerEntity player = (ClientPlayerEntity) (Object) this;
-      if (player.abilities.flying) {
-        final Input input = player.input;
-        if (!input.forward && !input.back && !input.left && !input.right) {
-          velocityX = 0.0;
-          velocityZ = 0.0;
-        }
-      }
-    }
+  private void inertiadampening$resetHorizontalVelocity(final CallbackInfo ci) {
+    InertiaDampening.resetHorizontalVelocity(this);
   }
 }
